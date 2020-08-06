7e0abc8a7e1b831504ae3d61157a0d41dadae870
sinarinzi-methode <methode@siriusschool.be>
2020-08-06 13:35:37 +0200

    actualisation
    mon code actualisé

---
+++ site-web-perso/script.js
@@ -0,0 +1,20 @@
+function ouvrirOnglet(evt, nomOnglet) {
+  // Declare tous les variables
+  var i, ongletContenu, ongletLinks;
+
+  // Pointer tous les éléments avec la class="ongletLinks" et les cacher
+  ongletContenu = document.getElementsByClassName("ongletContenu");
+  for (i = 0; i < ongletContenu.length; i++) {
+    ongletContenu[i].style.display = "none";
+  }
+
+  // Pointer tous les éléments avec la class="ongletLinks" et retirer la class "active"
+  ongletLinks = document.getElementsByClassName("ongletLinks");
+  for (i = 0; i < ongletLinks.length; i++) {
+    ongletLinks[i].className = ongletLinks[i].className.replace(" active", "");
+  }
+
+  // Afficher l'onglet courant, et ajouter la class "active" sur le bouton qui ouvre l'onglet
+  document.getElementById(nomOnglet).style.display = "block";
+  evt.currentTarget.className += " active";
+}
